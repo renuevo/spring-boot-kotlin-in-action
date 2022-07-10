7ec409c01e4ba72f32fa5ec6e54838df2385937a
renuevo <renuevo0110@gmail.com>
2022-07-11 00:00:41 +0900

    :art: add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,66 +2,63 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-  id("org.springframework.boot") version Versions.springBoot apply false
-  id("io.spring.dependency-management") version Versions.springDependencyManagement apply false
-  id("org.jlleitschuh.gradle.ktlint") version Versions.ktlint
-  kotlin("jvm") version Versions.kotlin
-  kotlin("plugin.spring") version Versions.kotlin
+    id("org.springframework.boot") version Versions.springBoot apply false
+    id("io.spring.dependency-management") version Versions.springDependencyManagement apply false
+    id("org.jlleitschuh.gradle.ktlint") version Versions.ktlint
+    kotlin("jvm") version Versions.kotlin
+    kotlin("plugin.spring") version Versions.kotlin
 }
 
 allprojects {
-  repositories {
-    mavenCentral()
-  }
+    repositories {
+        mavenCentral()
+    }
 
-  group = "com.github"
+    group = "com.github"
 }
 
 configurations {
-  compileOnly {
-    extendsFrom(configurations.annotationProcessor.get())
-  }
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
-
 
 subprojects {
 
-  apply(plugin = "kotlin")
-  apply(plugin = "kotlin-kapt")
-  apply(plugin = "kotlin-spring")
-  apply(plugin = "kotlin-allopen")
-  apply(plugin = "org.springframework.boot")
-  apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "kotlin")
+    apply(plugin = "kotlin-kapt")
+    apply(plugin = "kotlin-spring")
+    apply(plugin = "kotlin-allopen")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
 
-  java.sourceCompatibility = JavaVersion.VERSION_11
-  java.targetCompatibility = JavaVersion.VERSION_11
+    java.sourceCompatibility = JavaVersion.VERSION_11
+    java.targetCompatibility = JavaVersion.VERSION_11
 
-  dependencies {
+    dependencies {
 
-    implementation(Dependencies.jacksonModule)
-    implementation(Dependencies.kotlinReflect)
-    implementation(Dependencies.kotlinStdlibJdk8)
-
-  }
-
-  tasks.withType<KotlinCompile> {
-    kotlinOptions {
-      freeCompilerArgs = listOf("-Xjsr305=strict")
-      jvmTarget = "11"
+        implementation(Dependencies.jacksonModule)
+        implementation(Dependencies.kotlinReflect)
+        implementation(Dependencies.kotlinStdlibJdk8)
     }
-  }
 
-  tasks.withType<Test> {
-    useJUnitPlatform()
-    systemProperty("spring.profiles.active", "test")
-  }
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
 
-  tasks.named<Jar>("jar"){
-    enabled = true
-  }
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        systemProperty("spring.profiles.active", "test")
+    }
 
-  tasks.named<BootJar>("bootJar"){
-    enabled = false
-  }
+    tasks.named<Jar>("jar") {
+        enabled = true
+    }
 
+    tasks.named<BootJar>("bootJar") {
+        enabled = false
+    }
 }

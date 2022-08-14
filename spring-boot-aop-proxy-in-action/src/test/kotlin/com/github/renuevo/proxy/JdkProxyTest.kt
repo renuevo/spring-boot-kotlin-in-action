8e169e79bb6c358d14c71be6bfaa6260b18f8738
renuevo <renuevo0110@gmail.com>
2022-08-15 00:32:14 +0900

    :white_check_mark: jdk proxy test

--- spring-boot-aop-proxy-in-action/src/test/kotlin/com/github/renuevo/proxy/JdkProxyTest.kt
+++ spring-boot-aop-proxy-in-action/src/test/kotlin/com/github/renuevo/proxy/JdkProxyTest.kt
@@ -3,7 +3,6 @@ package com.github.renuevo.proxy
 import com.github.renuevo.proxy.domain.jdk.JdkProxyService
 import com.github.renuevo.proxy.domain.jdk.JdkProxyServiceImpl
 import io.kotest.core.spec.style.ShouldSpec
-import io.kotest.matchers.shouldBe
 import mu.KotlinLogging
 import java.lang.reflect.InvocationHandler
 import java.lang.reflect.Method
@@ -38,14 +37,14 @@ internal class TestInvocationHandler constructor(jdkProxyService: JdkProxyServic
     private val log = KotlinLogging.logger { }
     private val target = jdkProxyService
 
-    override fun invoke(proxy: Any, method: Method, args: Array<out Any>): Any {
+    override fun invoke(proxy: Any, method: Method, args: Array<out Any>?): Any? {
 
         when (method.name) {
             "methodA" -> log.info { "너는 A로 향하는군" }
             else -> log.info { "TestInvocationHandler 지나갑니다" }
         }
 
-        return method.invoke(target, args)  //실제 target 메서드 호출
+        return method.invoke(target, *(args ?: arrayOfNulls(0)))    //실제 target 호출
     }
 
 }

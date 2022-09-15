e906a2a410d5abb89333df294b78d8b12bad500c
renuevo <renuevo0110@gmail.com>
2022-09-16 01:26:56 +0900

    :sparkles: example proxyfactorybean

--- spring-boot-aop-proxy-in-action/src/main/kotlin/com/github/renuevo/proxy/domain/factory/config/CustomAdvisorConfig.kt
+++ spring-boot-aop-proxy-in-action/src/main/kotlin/com/github/renuevo/proxy/domain/factory/config/CustomAdvisorConfig.kt
@@ -1,8 +1,13 @@
 package com.github.renuevo.proxy.domain.factory.config
 
+import com.github.renuevo.proxy.domain.factory.FactoryCglibService
 import mu.KotlinLogging
+import org.aopalliance.aop.Advice
 import org.aopalliance.intercept.MethodInterceptor
 import org.aopalliance.intercept.MethodInvocation
+import org.springframework.aop.Advisor
+import org.springframework.aop.Pointcut
+import org.springframework.aop.framework.ProxyFactoryBean
 import org.springframework.aop.support.DefaultPointcutAdvisor
 import org.springframework.aop.support.NameMatchMethodPointcut
 import org.springframework.context.annotation.Bean
@@ -12,17 +17,26 @@ import org.springframework.context.annotation.Configuration
 class CustomAdvisorConfig {
 
     @Bean
-    fun targetPointcut() = NameMatchMethodPointcut().apply { addMethodName("proxyTarget*") }
+    fun targetPointcut(): Pointcut = NameMatchMethodPointcut().apply { addMethodName("method*") }
 
 
     @Bean
-    fun customAdvice() = CustomAdvice()
+    fun customAdvice(): Advice = CustomAdvice()
 
 
     @Bean
-    fun customAdvisor() = DefaultPointcutAdvisor(targetPointcut(), customAdvice())
+    fun customAdvisor(): Advisor = DefaultPointcutAdvisor(targetPointcut(), customAdvice())
+
+
+    @Bean
+    fun proxyFactoryBean() = ProxyFactoryBean().apply {
+        setTarget(ProxyFactoryBeanTarget())
+        setInterceptorNames("customAdvisor")
+    }
 
 }
+
+class ProxyFactoryBeanTarget : FactoryCglibService()
 
 
 class CustomAdvice : MethodInterceptor {
--- spring-boot-aop-proxy-in-action/src/test/kotlin/com/github/renuevo/proxy/SpringProxyBeanFactoryTest.kt
+++ spring-boot-aop-proxy-in-action/src/test/kotlin/com/github/renuevo/proxy/SpringProxyBeanFactoryTest.kt
@@ -2,22 +2,79 @@ package com.github.renuevo.proxy
 
 import com.github.renuevo.proxy.domain.factory.FactoryCglibService
 import com.github.renuevo.proxy.domain.factory.FactoryJdkProxyService
+import com.github.renuevo.proxy.domain.factory.FactoryJdkProxyServiceImpl
+import com.github.renuevo.proxy.domain.factory.config.ProxyFactoryBeanTarget
 import io.kotest.core.spec.style.ShouldSpec
 import mu.KotlinLogging
+import org.springframework.aop.Advisor
+import org.springframework.aop.framework.ProxyFactory
 import org.springframework.boot.test.context.SpringBootTest
 
 
 @SpringBootTest
 internal class SpringProxyBeanFactoryTest(
     private val factoryJdkProxyService: FactoryJdkProxyService,
-    private val factoryCglibService: FactoryCglibService
+    private val factoryCglibService: FactoryCglibService,
+    private val proxyFactoryBean: ProxyFactoryBeanTarget,
+    private val customAdvisor: Advisor
 ) : ShouldSpec({
 
     val log = KotlinLogging.logger { }
 
-    should("Factory Proxy Test") {
-        log.info { factoryJdkProxyService.javaClass.toString() }
-        log.info { factoryCglibService.javaClass.toString() }
+
+    context("Proxy Bean Factory Test") {
+
+        should("Factory Proxy Test") {
+            log.info { factoryJdkProxyService.javaClass.toString() }
+            log.info { factoryCglibService.javaClass.toString() }
+            log.info { proxyFactoryBean.javaClass.toString() }
+        }
+
+        should("Call Proxy Bean Factory Method") {
+            proxyFactoryBean.methodA()
+            proxyFactoryBean.methodB()
+            proxyFactoryBean.methodC()
+        }
+
+
+        should("JDK Proxy Factory Test") {
+            val proxy = ProxyFactory(FactoryJdkProxyServiceImpl()).apply {
+                addAdvisor(customAdvisor)
+            }.proxy as FactoryJdkProxyService
+
+            log.info { proxy.javaClass.toString() }
+
+            proxy.methodA()
+            proxy.methodB()
+            proxy.methodC()
+        }
+
+        should("CGlib Proxy Factory Test") {
+            val proxy = ProxyFactory(ProxyFactoryBeanTarget()).apply {
+                isProxyTargetClass = true
+                addAdvisor(customAdvisor)
+            }.proxy as ProxyFactoryBeanTarget
+
+            log.info { proxy.javaClass.toString() }
+
+            proxy.methodA()
+            proxy.methodB()
+            proxy.methodC()
+        }
+
+        should("Interface Target To CGLIB Proxy Test") {
+            val proxy = ProxyFactory(FactoryJdkProxyServiceImpl()).apply {
+                isProxyTargetClass = true
+                addAdvisor(customAdvisor)
+            }.proxy as FactoryJdkProxyService
+
+            log.info { proxy.javaClass.toString() }
+
+            proxy.methodA()
+            proxy.methodB()
+            proxy.methodC()
+        }
+
     }
 
 })

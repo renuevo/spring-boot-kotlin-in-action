4dc247957a7dce09bdb74e751c971feea31c1f57
renuevo <renuevo0110@gmail.com>
2022-09-23 22:41:03 +0900

    :tada: spring server to client event module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "spring-boot-kotlin-in-action"
 include("domain")
 include("spring-boot-jpa-in-action")
 include("spring-boot-aop-proxy-in-action")
+include("spring-boot-server-to-client-event-in-action")
---
+++ spring-boot-server-to-client-event-in-action/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+
+    implementation(project(Projects.domain))
+    implementation(Dependencies.springBootWeb)
+}
